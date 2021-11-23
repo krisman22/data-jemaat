68f65f7ee005a67e995375de0ff3e02d5b533a1e
unknown <adven.setiawan7@gmail.com>
2021-11-23 10:58:38 +0700

    adding colum nama alias Kepala keluarga export

---
+++ go-test/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello World")
+
+	// var firtsName string = "Adven"
+
+	// var lastName string
+	// lastName = "Setiawan"
+
+	// fmt.Printf("Hello %s %s!\n", firtsName, lastName)
+
+	name := new(string)
+
+	fmt.Println(name)
+}
